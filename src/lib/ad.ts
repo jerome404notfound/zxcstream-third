78672fdc8c9efad7c9640b7a5bd63918000020ab
iDaedalus <jerometecsonn@gmail.com>
2025-07-03 15:13:28 +0800

    first deploy

--- src/lib/ad.ts
+++ src/lib/ad.ts
@@ -2,13 +2,14 @@
 
 import { useCallback } from "react";
 
-let canOpenAd = false;
+let canOpenAd = true;
 
 export function useGlobalAdLink() {
   const adLink = useCallback(() => {
     if (!canOpenAd) return;
 
-    const adUrl = "https://theeghumoaps.com/4/8414760";
+    const adUrl =
+      "https://snowmansphereabrasive.com/pyepvwc4?key=3b8db78578d352ef8dfbf252e46812cd";
     window.open(adUrl, "_blank");
     canOpenAd = false;
 
